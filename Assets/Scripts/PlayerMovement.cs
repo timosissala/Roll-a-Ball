f2ce1fdc272c99ad9f9d8cca88db8662bf90f72d
timosissala <47850197+timosissala@users.noreply.github.com>
2020-05-23 18:19:34 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    [SerializeField]
+    private float speed = 1;
+
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UserInput();
+        Movement();
+    }
+
+    private void UserInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void Movement()
+    {
+        float xForce = Input.GetAxis("Horizontal");
+        float zForce = Input.GetAxis("Vertical");
+
+        Vector3 forceVector = new Vector3(xForce * speed, 0, zForce * speed);
+        rb.AddForce(forceVector);
+    }
+
+    private void Jump()
+    {
+        Vector3 jumpVector = new Vector3(0, 200, 0);
+
+        rb.AddForce(jumpVector);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Pickup")
+        {
+            score++;
+            Debug.Log("Score: " + score);
+            Destroy(other.gameObject);
+        }
+    }
+}
